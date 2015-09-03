59cf363705762f4247d8f8c7dfdd6c9f6b74bf44
LukeHaines <lukehaines2@gmail.com>
2015-09-03 07:38:27 +0100

    file structure agreed

--- app.rb
+++ app.rb
@@ -12,14 +12,40 @@ end
 post '/navigate' do
   case params[:destination].downcase
   when 'Basic Calc' then redirect to '/basic'
-  when 'Advanced Calc' then redirect to '/advanced'
-  when 'BMI' then redirect to '/BMI'
+  when 'advanced calc' then redirect to '/advanced'
+  when 'bmi' then redirect to '/BMI'
   when 'trip' then redirect to '/trip'
   when 'mortgage' then redirect to '/mortgage'
   else
     @error = 'Invalid page selected you moron!'
     erb :home
   end
+end
+
+
+get '/basic' do
+  @title = 'Basic Calc'
+  erb :basic
+end
+
+get '/advanced' do
+  @title = 'Advanced Calc'
+  erb :advanced
+end
+
+get '/bmi' do
+  @title = 'BMI'
+  erb :bmi
+end
+
+get '/trip' do
+  @title = 'trip'
+  erb :trip
+end
+
+get '/mortgage' do
+  @title = 'mortgage'
+  erb :mortgage
 end
 
 
